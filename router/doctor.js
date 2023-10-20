9148c5c6c6402435fa790459fef1b563d45e99af
Riya Joshy <144624347+rikeshiya@users.noreply.github.com>
2023-10-20 10:33:19 +0530

    Delete router/doctor.js

--- router/doctor.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const doctor = require('../controller/index');
-router.post('/register', doctor.Register);
-router.get('/login', doctor.Login);
-module.exports = router;
