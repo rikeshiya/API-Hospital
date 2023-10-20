80b8609603d71b1f571becaa6d19d81c87e1672e
Riya Joshy <144624347+rikeshiya@users.noreply.github.com>
2023-10-20 10:33:36 +0530

    Delete router/patient.js

--- router/patient.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../config/auth');
-const patient = require('../controller/patient');
-router.post('/registers',patient.register);
-module.exports = router;
