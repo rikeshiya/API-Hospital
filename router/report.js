0088588a021e0a600354a63b8471b4cc3b3ef11a
Riya Joshy <144624347+rikeshiya@users.noreply.github.com>
2023-10-20 10:33:43 +0530

    Delete router/report.js

--- router/report.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../config/auth');
-const createReport = require('../controller/report');
-router.post('/:id/create_report' ,auth.checkAuthentication, createReport.createReport);
-router.get('/:id/all_report' ,auth.checkAuthentication, createReport.allReport);
-router.get('/:status' , createReport.status);
-module.exports = router;
